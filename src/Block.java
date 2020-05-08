fede7c41d16443ddbaaec52deb07def287447479
Krokiet <kjksosnowski@gmail.com>
2020-05-08 14:14:04 +0200

    chunk

--- src/Block.java
+++ src/Block.java
@@ -11,10 +11,10 @@ public abstract class Block {
 
     public float[] getData(float x, float y, float z) {
         return new float[] {
-                x, y, z,                            color.r, color.g, color.b, 1,
-                x + size, y, z,                     color.r, color.g, color.b, 1,
-                x + size, y + size, z,              color.r, color.g, color.b, 1,
-                x, y + size, z,                     color.r, color.g, color.b, 1,
+                x, y, z,                            0, 0, 0, 1,
+                x + size, y, z,                     1, 0, 0, 1,
+                x + size, y + size, z,              1, 0, 1, 1,
+                x, y + size, z,                     0, 1, 0, 1,
 
                 x, y, z + size,                     color.r, color.g, color.b, 1,
                 x + size, y, z + size,              color.r, color.g, color.b, 1,
@@ -22,19 +22,19 @@ public abstract class Block {
                 x, y + size, z + size,              color.r, color.g, color.b, 1,
 
                 x, y, z,                            color.r, color.g, color.b, 1,
-                x, y + size, z,                     color.r, color.g, color.b, 1,
+                x, y + size, z,                     color.r, color.g*0.5f, color.b, 1,
                 x, y + size, z + size,              color.r, color.g, color.b, 1,
                 x, y, z + size,                     color.r, color.g, color.b, 1,
 
-                x + size, y, z,                     color.r, color.g, color.b, 1,
+                x + size, y, z,                     color.r, color.g*0.5f, color.b, 1,
                 x + size, y + size, z,              color.r, color.g, color.b, 1,
-                x + size, y + size, z + size,       color.r, color.g, color.b, 1,
+                x + size, y + size, z + size,       color.r, color.g*0.5f, color.b, 1,
                 x + size, y, z + size,              color.r, color.g, color.b, 1,
 
-                x, y, z,                            color.r, color.g, color.b, 1,
+                x, y, z,                            color.r, color.g*0.5f, color.b, 1,
                 x + size, y, z,                     color.r, color.g, color.b, 1,
                 x + size, y, z + size,              color.r, color.g, color.b, 1,
-                x, y, z + size,                     color.r, color.g, color.b, 1,
+                x, y, z + size,                     color.r, color.g*0.5f, color.b, 1,
 
                 x, y + size, z,                     color.r, color.g, color.b, 1,
                 x + size, y + size, z,              color.r, color.g, color.b, 1,
