7900dbc2d725a491c0fed7bab3287c465cf5bbec
jankoksik <jawobo123@gmail.com>
2020-05-09 10:57:29 +0200

    Inventory System, poczatki * dodawanie itemow * ustalanie wilekosci eq (postaram sie zeby np. skrzynie mogly z tego korzystac)

--- src/Test.java
+++ src/Test.java
@@ -6,6 +6,7 @@ import org.joml.Vector3f;
 import org.json.simple.parser.ParseException;
 import org.lwjgl.opengl.GL;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
@@ -13,8 +14,9 @@ import java.util.Map;
 public class Test {
 
     Chunk stefan;
+    float angle;
 
-    public Test() {
+    public Test(){
         if (glfwInit() != true){
             System.err.println("GLFW sie zepsul");
             System.exit(1);
@@ -23,7 +25,7 @@ public class Test {
         final int WIDTH = 640*2;
         final int HEIGHT = 480*2;
 
-        long win = glfwCreateWindow(WIDTH, HEIGHT, "APROCraft v" + APROCraft.VERSION, 0,0);
+        long win = glfwCreateWindow(WIDTH, HEIGHT, "Minecraft v.0.0.1", 0,0);
 
         /* DEFAULT SETTINGS
         //MOUSE
@@ -55,6 +57,32 @@ public class Test {
         }
 
          */
+        //Przykladowy ekwipunek z proba przeciazenia
+        TestLogLabel("Eq add test");
+        Inventory eqi = new Inventory(3, 10);
+        if(eqi.addItem(0)){System.out.println("dodano pomyslnie item");}else {System.out.println("blad");}
+        // if(eqi.addItem(0)){System.out.println("dodano pomyslnie item");}else {System.out.println("blad");}
+        // if(eqi.addItem(0)){System.out.println("dodano pomyslnie item");}else {System.out.println("blad");}
+         //if(eqi.addItem(1)){System.out.println("dodano pomyslnie item");}else {System.out.println("blad");}
+        // if(eqi.addItem(2)){System.out.println("dodano pomyslnie item");}else {System.out.println("blad");}
+        // if(eqi.addItem(0)){System.out.println("dodano pomyslnie item");}else {System.out.println("blad");}
+        // if(eqi.addItem(1)){System.out.println("dodano pomyslnie item");}else {System.out.println("blad");}
+        // if(eqi.addItem(0)){System.out.println("dodano pomyslnie item");}else {System.out.println("blad");}
+        // if(eqi.addItem(3)){System.out.println("dodano pomyslnie item");}else {System.out.println("blad");}
+
+        TestLogLabel("Eq add many");
+        if(eqi.addItem(0, 7, true)){System.out.println("dodano pomyslnie item");}else {System.out.println("blad");}
+
+
+        // testowe wypisanie eq
+
+        TestLogLabel("Eq content");
+        for(Item i : eqi.getEq())
+        {
+            System.out.println(i.getId() + " : " + i.getSize());
+        }
+        TestLogLabel("Controls read");
+
         try {
             HashMap<String, Integer>  cntrl = SaveNReadJson.readControls("Controls");
             SaveNReadJson.applyCOntrols(cntrl);
@@ -69,6 +97,8 @@ public class Test {
             e.printStackTrace();
         }
 
+
+
         glfwShowWindow(win);
         glfwMakeContextCurrent(win);
         GL.createCapabilities();
@@ -80,7 +110,11 @@ public class Test {
 
         //Texture dirt = new Texture("./resources/DirtGrassSide.png");
 
+
+        glEnable(GL_DEPTH_TEST);
+
         stefan = new Chunk(0,0);
+        angle = 0;
 
         float[] vertices =  new float[]{
                 -0.5f, 0.5f, 0, // gora lewo 0
@@ -112,108 +146,82 @@ public class Test {
         float Zcam = -5;
         float rotatey = 0;
         float yRot = 0;
-
-
-        long timePrev = System.nanoTime();
-        long timer = System.currentTimeMillis();
-        double ns = 1_000_000_000 / 60.0;
-
-        int frames = 0;
-        int ticks = 0;
-
-        glEnable(GL_DEPTH_TEST);
-
         while(glfwWindowShouldClose(win) != true)
         {
+            target = scale;
+            if(glfwGetKey(win, GLFW_KEY_ESCAPE)==GL_TRUE)
+            {
+                glfwSetWindowShouldClose(win, true);
+            }
+            if(glfwGetKey(win, Controls.getUp())==GL_TRUE)
+            {
+                Ycam -= 0.01f;
+            }
+            if(glfwGetKey(win, Controls.getDown())==GL_TRUE)
+            {
+                Ycam += 0.01f;
+            }
+            if(glfwGetKey(win, Controls.getRight())==GL_TRUE)
+            {
+                Xcam -= 0.01f;
+                //yRot -= 0.01f;
+            }
+            if(glfwGetKey(win,Controls.getLeft())==GL_TRUE)
+            {
+                Xcam += 0.01f;
+                //yRot -= 0.01f;
+            }
+            if(glfwGetKey(win, Controls.getBackward())==GL_TRUE)
+            {
+                Zcam -= 0.01f;
+            }
+            if(glfwGetKey(win, Controls.getForward())==GL_TRUE)
+            {
+                Zcam += 0.01f;
+            }
+            if(glfwGetKey(win, GLFW_KEY_LEFT)==GL_TRUE)
+            {
+                camera.addRotation(0,-0.001f, 0);
+                yRot -= 0.1f;
+            }
+            if(glfwGetKey(win, GLFW_KEY_RIGHT)==GL_TRUE)
+            {
+               camera.addRotation(0,0.001f, 0);
+                yRot += 0.1f;
+            }
+            if(glfwGetKey(win, GLFW_KEY_R)==GL_TRUE)
+            {
+               // camera.setPosition(new Vector3f(Xcam, Ycam, Zcam));
+                yRot+=0.5f;
+                //camera.setRotation(new Vector3f(0,rotatey + 90,0));
+            }
+
+            camera.setPosition(new Vector3f(Xcam, Ycam, Zcam));
+            camera.setRotation(new Vector3f(0,rotatey,0));
+
             glfwPollEvents();
 
-            long timeNow = System.nanoTime();
-            double elapsed = timeNow - timePrev;
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-            if (elapsed > ns) {
-                if(glfwGetKey(win, GLFW_KEY_ESCAPE)==GL_TRUE)
-                {
-                    glfwSetWindowShouldClose(win, true);
-                }
-                if(glfwGetKey(win, Controls.getUp())==GL_TRUE)
-                {
-                    Ycam -= 0.01f;
-                }
-                if(glfwGetKey(win, Controls.getDown())==GL_TRUE)
-                {
-                    Ycam += 0.01f;
-                }
-                if(glfwGetKey(win, Controls.getRight())==GL_TRUE)
-                {
-                    Xcam -= 0.01f;
-                    //yRot -= 0.01f;
-                }
-                if(glfwGetKey(win,Controls.getLeft())==GL_TRUE)
-                {
-                    Xcam += 0.01f;
-                    //yRot -= 0.01f;
-                }
-                if(glfwGetKey(win, Controls.getBackward())==GL_TRUE)
-                {
-                    Zcam -= 0.01f;
-                }
-                if(glfwGetKey(win, Controls.getForward())==GL_TRUE)
-                {
-                    Zcam += 0.01f;
-                }
-                if(glfwGetKey(win, GLFW_KEY_LEFT)==GL_TRUE)
-                {
-                    camera.addRotation(0,-0.001f, 0);
-                    yRot += 0.5f;
-                }
-                if(glfwGetKey(win, GLFW_KEY_RIGHT)==GL_TRUE)
-                {
-                    camera.addRotation(0,0.001f, 0);
-                    yRot -= 0.5f;
-                }
-                if(glfwGetKey(win, GLFW_KEY_R)==GL_TRUE)
-                {
-                    camera.setPosition(new Vector3f(Xcam, Ycam, Zcam));
-                    camera.setRotation(new Vector3f(0,rotatey + 90,0));
-                }
+            glMatrixMode(GL_PROJECTION);
+            glLoadIdentity();
+            gluPerspective(70.0f, ((float)WIDTH/(float)HEIGHT), 0.1f, 1000.0f);
+            glMatrixMode(GL_MODELVIEW);
+            glLoadIdentity();
 
-                camera.setPosition(new Vector3f(Xcam, Ycam, Zcam));
-                camera.setRotation(new Vector3f(0,rotatey,0));
+            glTranslatef(Xcam, Ycam, Zcam);
+            glRotatef(yRot, 0, 1, 0);
+            //glRotatef(angle, 0, 1, 0);
+            //angle++;
 
-                ticks ++;
-                timePrev += ns;
-            } else {
-                glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-                glClearColor(0.1f, 0.2f, 0.7f, 1);
+            //shader.bind();
+            //shader.setUniform("sampler", 0);
+            //shader.setUniform("projection", camera.getViewMatrix());
+            //dirt.bind(0);
+            //model.render();
+            stefan.render();
 
-                glMatrixMode(GL_PROJECTION);
-                glLoadIdentity();
-                gluPerspective(70.0f, ((float)WIDTH/(float)HEIGHT), 0.1f, 1000.0f);
-                glMatrixMode(GL_MODELVIEW);
-                glLoadIdentity();
-
-                glTranslatef(Xcam, Ycam, Zcam);
-                glRotatef(yRot, 0, 1, 0);
-
-                //shader.bind();
-                //shader.setUniform("sampler", 0);
-                //shader.setUniform("projection", camera.getViewMatrix());
-                //dirt.bind(0);
-                //model.render();
-                stefan.render();
-                frames++;
-                glfwSwapBuffers(win);
-            }
-
-            if (System.currentTimeMillis() - timer > 1000) {
-                System.out.println("FPS: " + frames + " UPS: " + ticks);
-                frames = 0;
-                ticks = 0;
-                timer += 1000;
-            }
-
-
-            /*glColor3f(0.5f,0.5f,1.0f);
+            glColor3f(0.5f,0.5f,1.0f);
 
             glBegin(GL_QUADS);
             glColor3f(1.0f,1.0f,0.0f);
@@ -246,9 +254,9 @@ public class Test {
             glVertex3f( 1.0f, 1.0f, 1.0f);
             glVertex3f( 1.0f,-1.0f, 1.0f);
             glVertex3f( 1.0f,-1.0f,-1.0f);
-            glEnd();*/
+            glEnd();
 
-            //glfwSwapBuffers(win);
+            glfwSwapBuffers(win);
         }
         glfwTerminate();
 
@@ -260,6 +268,16 @@ public class Test {
         float left = aspect * bottom;
         float right = -left;
         glFrustum(left, right, bottom, top, near, far);
+    }
+
+    public static void TestLogLabel(String Tested)
+    {
+        System.out.println();
+        System.out.println("+---------------------------------------------");
+        System.out.println("| Test : " + Tested);
+        System.out.println("+---------------------------------------------");
+        System.out.println();
+
     }
 
     public static void main(String [] args){
