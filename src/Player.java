43b46b55730f2e8f91dd31df5efa15d88c24d5b9
Krokiet <kjksosnowski@gmail.com>
2020-05-12 21:44:12 +0200

    ladniejsze zaznaczenie

--- src/Player.java
+++ src/Player.java
@@ -297,6 +297,9 @@ public class Player {
         if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_1) == GLFW_PRESS)
             world.setBlock((int)x, (int)y, (int)z, Blocks.AIR);
 
+        glLineWidth(2);
+        glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
         glBegin(GL_QUADS);
 
         glVertex3f(x, y, z);
@@ -330,6 +333,8 @@ public class Player {
         glVertex3f(x + s, y + s, z + s);
 
         glEnd();
+
+        glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
     }
 
     public float getX() {
