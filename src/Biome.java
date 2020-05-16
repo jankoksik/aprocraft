165161f155543741ee9f8ec049f79b825e9cbbb8
Krokiet <kjksosnowski@gmail.com>
2020-05-16 13:12:38 +0200

    Biomy (poczatek)

--- src/Biome.java
+++ src/Biome.java
@@ -7,6 +7,21 @@ public abstract class Biome {
     public static final int PLAINS = 3;
     public static final int MOUNTAINS = 4;
 
-    private List<Structures> vegetation;
-    private List<Structures> buildings;
+    private String name;
+
+    private int amplitude;
+    private int octave;
+
+    private List<Block> layer;
+    private List<Block> ores;
+
+    private List<Structure> vegetation;
+    private List<Structure> buildings;
+
+    private float vegetationDestiny;
+    private float buildingsDestiny;
+
+    public Biome(String name) {
+        this.name = name;
+    }
 }
