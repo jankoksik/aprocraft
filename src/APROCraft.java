3ce21c04c6a3e7ae35c477db2d167825ce9c7f6c
jankoksik <jawobo123@gmail.com>
2020-05-20 15:57:06 +0200

    powinny być cyferki :c

--- src/APROCraft.java
+++ src/APROCraft.java
@@ -158,7 +158,7 @@ public class APROCraft {
 
                 player.getGui().RenderHealth(player.getHp());
                 player.getGui().RenderQAB();
-                player.getGui().RenderEq();
+                player.getGui().RenderEq() ;
 
                 //GUI.Test();
                 //glColor3f(1f, .5f, 1f);
